d06fe97c7ed66d18105fa82905df3171bdd1bb9c
doudoufeng <1325395157@qq.com>
2020-04-27 20:26:50 +0800

    脚手架和Ant Design

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import {Button} from "antd";
+
+export default class MyComponent extends Component{
+  render(){
+    return (
+      <div>
+          App...
+          <Button type="primary">点击</Button>
+      </div>
+    )
+  }
+}
