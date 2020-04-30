f6da936a7e281e71ae0c532a04f619125a1bb8b9
doudoufeng <1325395157@qq.com>
2020-04-30 10:23:59 +0800

    redux_all完成

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import store from "./redux/store";
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
-// 如果redux状态发生变化 就调用store.subscribe
-store.subscribe(()=>{
-    ReactDOM.render(<App />, document.getElementById('root'))
-})
---
+++ src_redux_all_nstandard/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react'
+import Count from "./Components/Count";
+
+export default class App extends Component{
+  render(){
+    return (
+      <div>
+        <Count />
+      </div>
+    )
+  }
+}
